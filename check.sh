c0c5585a087f0844ffb1b35842483d7d0f918efd
Andy Ward.uk <andy.ward.uk@gmail.com>
2022-11-26 00:25:46 +0000

    Format check not required

--- check.sh
+++ check.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 cargo clippy --color always -q 2>&1 | less -R
-cargo fmt --check --all 2>&1 | less -R
 cargo check --color always -q 2>&1 | less -R
 cargo test --color always 2>&1 | less -R
