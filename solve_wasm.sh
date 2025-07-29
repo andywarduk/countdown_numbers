0cb9d83769cf7200421df1be1f8de551d2b51801
Andy Ward.uk <andy.ward.uk@gmail.com>
2025-07-29 16:37:53 +0100

    wasmp1

--- solve_wasm.sh
+++ solve_wasm.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-# relies on cargo-wasi being installed: cargo install cargo-wasi [--force]
 # target installed with: rustup target add wasm32-wasi
 
-cargo wasi run --quiet --release --bin solve -- $@ 2>&1 | less -R
+cargo build --quiet --release --bin solve --target wasm32-wasip1
+wasmtime --dir=. target/wasm32-wasip1/release/solve.wasm -- $@ 2>&1 | less -R
+
